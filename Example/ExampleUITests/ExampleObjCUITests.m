ef2ace923d08bd61b39e432dd63a909694237d80
Michael Eisel <michael.eisel@gmail.com>
2022-07-15 16:02:39 -0400

    Initial

--- Example/ExampleUITests/ExampleObjCUITests.m
+++ Example/ExampleUITests/ExampleObjCUITests.m
@@ -1,6 +1,24 @@
 #import <XCTest/XCTest.h>
 #import <EMGPerfTesting/EMGPerfTesting-Swift.h>
 
+@interface ObjcPerfTest : NSObject <EMGPerfTest>
+
+@end
+
+@implementation ObjcPerfTest
+
+- (void)runInitialSetupWithApp:(XCUIApplication *)app
+{
+    NSLog(@"objc setup");
+}
+
+- (void)runIterationWithApp:(XCUIApplication *)app
+{
+    NSLog(@"objc iteration");
+}
+
+@end
+
 @interface ExampleObjCUITests : XCTestCase
 
 @end
