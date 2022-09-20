e9009aa5df843fcef266a8b320d59bf3c4fd9ce7
Michael Eisel <michael.eisel@gmail.com>
2022-09-20 15:44:00 -0400

    Misc fixes

--- Example/ExampleUITests/ExampleUITests.swift
+++ Example/ExampleUITests/ExampleUITests.swift
@@ -2,12 +2,10 @@ import XCTest
 import EMGPerfTesting
 
 class ExamplePerfTest: NSObject, EMGPerfTest {
-    func runInitialSetup(withApp app: XCUIApplication) {
-        app.launch()
+    func runInitialSetup(withApp app: XCUIElement) {
     }
 
-    func runIteration(withApp app: XCUIApplication) {
-        app.launch()
+    func runIteration(withApp app: XCUIElement) {
         app.buttons["Button"].tap()
         app.alerts["Title"].buttons["OK"].tap()
     }
