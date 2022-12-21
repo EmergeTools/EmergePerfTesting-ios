57f8fdd10e0c9c7a48df658b7374f4a849489cab
Trevor Elkins <trevor@emergetools.com>
2022-12-21 16:57:08 -0500

    Cleanup

--- PerfTest.swift
+++ PerfTest.swift
@@ -2,6 +2,7 @@ import Foundation
 import XCTest
 
 @objc public protocol EMGPerfTest: NSObjectProtocol {
+  
     /// This setup function is run once before all the iterations of the perf test. It can be used, for example, to
     /// log a test user in before doing some perf test that requires a logged in state. Do _not_ create an instance
     /// of XCUIApplication for the target app (i.e., XCUIApplication()) yourself. Only use the one provided, `app`.
--- PerfTestRunner.swift
+++ PerfTestRunner.swift
@@ -2,6 +2,7 @@ import UIKit
 import XCTest
 
 @objc public class PerfTestRunner: NSObject {
+  
     @objc public static func runAllPerfTestsForBundle(ofClass classInBundle: AnyClass) {
         let imageName = class_getImageName(classInBundle)!
         var outCount: UInt32 = 0
@@ -9,26 +10,32 @@ import XCTest
         for i in 0..<Int(outCount) {
             let className = classNames[i]
             let aClass = objc_getClass(className)
+          
             if let perfTestClass = aClass as? EMGPerfTest.Type {
                 let objcClass = perfTestClass as! NSObject.Type
                 let test = objcClass.init() as! EMGPerfTest
                 print("Testing perf test class \(String(describing: aClass))")
                 print("Running initial setup for \(String(describing: aClass))")
-                let setupApp = XCUIApplication()
-                setupApp.launchEnvironment = test.launchEnvironmentForSetup?() ?? [:]
-                setupApp.launchArguments = test.launchArgumentsForSetup?() ?? []
-                setupApp.launch()
+                
+                let setupApp = makeApplication(forTest: test)
                 test.runInitialSetup(withApp: setupApp)
+              
                 print("Running two iterations for \(String(describing: aClass))")
                 for i in 0..<2 {
                     print("Iteration \(i + 1)")
-                    let app = XCUIApplication()
-                    app.launchEnvironment = test.launchEnvironmentForIterations?() ?? [:]
-                    app.launchArguments = test.launchArgumentsForIterations?() ?? []
-                    app.launch()
+                    let app = makeApplication(forTest: test)
                     test.runIteration(withApp: app)
                 }
             }
         }
     }
+    
+    private static func makeApplication(forTest test: EMGPerfTest) -> XCUIApplication {
+        let setupApp = XCUIApplication()
+        setupApp.launchEnvironment = test.launchEnvironmentForSetup?() ?? [:]
+        setupApp.launchArguments = test.launchArgumentsForSetup?() ?? []
+        setupApp.launch()
+        return setupApp
+    }
+  
 }
